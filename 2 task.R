f1acbe373267d068796ea5ee1c6c5d7af11945d6
niki8885 <63238384+niki8885@users.noreply.github.com>
2020-11-20 12:04:48 +0100

    Update 2 task.R

--- 2 task.R
+++ 2 task.R
@@ -1,10 +1,37 @@
-cacheSolve <- function(x, ...) {
-  inv <- x$getInverse()
-  if(!is.null(inv)){
-    return(inv)
+makeCacheMatrix <- function(m_in = matrix()) {
+    #Creating a NULL Matrix
+    m <- NULL
+
+    #Exporting the Matrix  
+  matrix_set <- function(m_out) {
+          m_in <<- m_out
+          m <<- NULL
   }
-  data <- x$get()
-  inv <- solve(data)
-  x$setInverse(inv)
-  inv      
+  
+  #Defining the return matrix 
+  matrix_get <- function() m_in
+
+  #Getting the Inverse Matrix
+  matrix_set_Inverse <- function(inverse) m <<- inverse
+  matrix_get_inverse <- function() m
+  
+  #Creating the list of objects 
+  list(matrix_set = matrix_set,
+       matrix_get = matrix_get,
+       matrix_set_Inverse = matrix_set_Inverse,
+       matrix_get_inverse = matrix_get_inverse)
 }
+
+cacheSolve <- function(m_in, ...) {
+  
+  # Setting Inverted Matrix 
+  m <- m_in$matrix_get_inverse()
+  #Checking if the matrix was already computed
+  if (!is.null(m)) {
+          message("Getting cached Matrix")
+          return(m)
+  }
+  data <- m_in$matrix_get()
+  m <- solve(data, ...)
+  m_in$matrix_set_Inverse(m)
+  m
